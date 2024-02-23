7bc1f533c2353812fc210910aa5781ce33fcf00e
si-hammad-ul-hassan <hammadmansha136@gmail.com>
2024-02-23 12:32:30 +0500

    explore image done

--- lib/controller/explore/explore_controller.dart
+++ lib/controller/explore/explore_controller.dart
@@ -6,10 +6,12 @@ import 'package:http/http.dart' as http;
 
 import '../../models/posts_model/posts_model.dart';
 
-class ExploreController extends GetxController {
+class ExploreController extends GetxController with InitializeLocalStorage{
   RxBool isLoading = false.obs;
   late Future<List<FeedPostData>?> feedsFuture;
   RxList<bool> likedList = List.generate(500, (index) => false).obs;
+  String myUserId="";
+
 
   String formattedDate(String timestamp) {
     // Parse the timestamp string into a DateTime object
@@ -17,6 +19,7 @@ class ExploreController extends GetxController {
 
     // Format the DateTime object to display only the date
     String formattedDate = DateFormat('yyyy-MM-dd').format(dateTime);
+
 
     return formattedDate;
   }
@@ -29,13 +32,16 @@ class ExploreController extends GetxController {
   @override
   void onInit() async {
     feedsFuture = getFeeds();
+    myUserId=storage.read("userId");
     super.onInit();
   }
 
   Future<List<FeedPostData>?> getFeeds() async {
     try {
-      // isLoading.value = true;
-      Uri url = Uri.parse(ApiData.getFeedPost);
+       isLoading.value = true;
+       feedsFuture = Future.value([]);
+
+       Uri url = Uri.parse(ApiData.getFeedPost);
       if (kDebugMode) {
         print("Get user request------------$url");
       }
@@ -45,22 +51,102 @@ class ExploreController extends GetxController {
       print("response of explore call==============${res.body}");
 
       if (res.statusCode == 200) {
-        final List body = json.decode(res.body);
+        List body = json.decode(res.body);
+        body=body.reversed.toList();
+        isLoading.value=false;
+
         return body.map((e) => FeedPostData.fromJson(e)).toList();
-      } else if (res.statusCode == 401) {
-        CommonToast.showToast(AppStrings.incorrectCredentials);
-      } else {
+      }  else {
         CommonToast.showToast(AppStrings.somethingWentWrong);
+        isLoading.value=false;
+
       }
     } on TimeoutException catch (e) {
       CommonToast.showToast(AppStrings.unableToConnect);
+      isLoading.value=false;
+
       print("Request timed out: $e");
     } on http.ClientException catch (e) {
       CommonToast.showToast(AppStrings.connectionNotStable);
+      isLoading.value=false;
+
       print("Client-side error occurred: $e");
     } catch (e) {
+      isLoading.value=false;
+
       print("Error occurred during request: $e");
     }
     return null;
   }
+
+
+  Future<void> followUser(String followerId) async {
+    try {
+      isLoading.value = true;
+
+      Uri url = Uri.parse(ApiData.followUser);
+      if (kDebugMode) {
+        print("Follow user request------------$url");
+      }
+
+      var body = jsonEncode({
+        "follower_id": followerId,
+        "user_id": myUserId,
+      });
+
+      var res = await http
+          .post(
+        url,
+        body: body,
+      )
+          .timeout(const Duration(seconds: 20));
+
+      print("response of follow==============${res.statusCode}");
+      print("response of follow==============$body");
+      print("response of follow==============${res.body}");
+
+      if (res.statusCode == 200) {
+        var data = json.decode(res.body);
+
+        CommonToast.showToast(AppStrings.loginSuccess);
+        isLoading.value = false;
+
+
+        update();
+      }
+
+      else if (res.statusCode == 401) {
+        CommonToast.showToast(AppStrings.incorrectCredentials);
+
+        isLoading.value = false;
+        update();
+      } else {
+        CommonToast.showToast(AppStrings.somethingWentWrong);
+        isLoading.value = false;
+        update();
+      }
+    } on TimeoutException catch (e) {
+      // Handle timeout exception
+      CommonToast.showToast(AppStrings.unableToConnect);
+
+      isLoading.value = false;
+      update();
+      print("Request timed out: $e");
+    } on http.ClientException catch (e) {
+      CommonToast.showToast(AppStrings.connectionNotStable);
+      // Handle client-side exceptions (e.g., internet connection issues)
+      print("Client-side error occurred: $e");
+      isLoading.value = false;
+      update();
+    } catch (e) {
+      print("Error occurred during request: $e");
+      isLoading.value = false;
+      update();
+    }
+  }
+
+
+
+
+
 }
